ce91a5b5803a80b8f2a66997d967e5c01edc522c
fr-kan5 <manwith1713@gmail.com>
2021-01-29 15:41:58 +0900

    投稿機能の実装

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,8 +3,21 @@ class AlbumsController < ApplicationController
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :caption, :image).merge(user_id: current_user.id)
   end
 end
