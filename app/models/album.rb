5c21663242a0f2ea03d59f9c747d4119db15ac13
fr-kan5 <manwith1713@gmail.com>
2021-01-31 03:05:50 +0900

    Commentモデルとテーブルの作成とバリデーションの設定

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :caption, presence: true
