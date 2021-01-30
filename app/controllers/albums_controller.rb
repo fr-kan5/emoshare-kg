832f95fe896a81273ba93b251c9e489b540e6b86
fr-kan5 <manwith1713@gmail.com>
2021-01-30 19:34:14 +0900

    編集機能の実装

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,5 @@
 class AlbumsController < ApplicationController
+  before_action :album_find, except: [:index, :new, :create]
   def index
     @albums = Album.includes(:user)
   end
@@ -17,18 +18,26 @@ class AlbumsController < ApplicationController
   end
 
   def show
-    @album = Album.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @album.update(album_params)
+      redirect_to album_path(@album)
+    else
+      render :edit
+    end
   end
 
   private
 
   def album_params
     params.require(:album).permit(:title, :caption, :image).merge(user_id: current_user.id)
+  end
+
+  def album_find
+    @album = Album.find(params[:id])
   end
 end
