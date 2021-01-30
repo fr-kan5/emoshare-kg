ce6cbc437c0057f5b9618c60731c350b613282e4
fr-kan5 <manwith1713@gmail.com>
2021-01-31 03:14:47 +0900

    コントローラーの作成とアクションとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
-  resources :albums, only: [:new, :create, :show, :edit, :update, :destroy]
+  
+  resources :albums, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
 end
