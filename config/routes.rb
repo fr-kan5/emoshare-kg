ab30bac1bdd1aad47c3f06ac3122ccb3e5c1f1fa
fr-kan5 <manwith1713@gmail.com>
2021-01-30 19:09:00 +0900

    edit,updateアクションとルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
-  resources :albums, only: [:new, :create, :show]
+  resources :albums, only: [:new, :create, :show, :edit, :update]
 end
