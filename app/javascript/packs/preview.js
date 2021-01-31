e2f5481667cd2c9a1c9749ce5216714f971ac7b0
fr-kan5 <manwith1713@gmail.com>
2021-02-01 05:43:02 +0900

    選択した画像情報の取得

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
+  document.getElementById('album_image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+  });
 });
